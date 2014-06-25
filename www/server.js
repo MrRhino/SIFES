9bd9806bd836da6db51ae0a9d367b387cadc1a44
ulrik.sorber <ulrik.sorber@gmail.com>
2014-06-26 01:26:31 +0200

    Changing to serve-static

--- www/server.js
+++ www/server.js
@@ -1,9 +1,9 @@
-var connect    = require('connect');
-var server     = connect.createServer();
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var app = connect();
+app.use(serveStatic(__dirname));
 
-var port       = process.env.PORT || 9796;
-
-server.use(connect.static(__dirname));
-server.listen(port);
+var port       = process.env.PORT || 8080;
+app.listen(port);
 
 console.log('Accepting connections on port ' + port + '...');
